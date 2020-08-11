6aafcaab36d5823754e68d6de5a877bbe1eadc10
krishna5816 <krishna5816@github.com>
2020-08-11 19:58:07 +0545

    title removed

--- resources/views/layouts/back/index.blade.php
+++ resources/views/layouts/back/index.blade.php
@@ -34,42 +34,14 @@
                 <div class="logo">
 
                     <a href="#" class="simple-text logo-normal" style="margin-left:1.5rem;">
-                        Urban Research Photography
+                        Urban Research<br> Photography
                     </a>
                 </div>
                 <div class="user">
                     <div class="photo">
                         {{-- <img src="{{ URL::to('back/img/surya.png') }}" /> --}}
                     </div>
-                    <div class="info ">
-                        <a data-toggle="collapse" href="#collapseExample" class="collapsed">
-                            <span>{{  Auth::user()->name }}
-                                <b class="caret"></b>
-                            </span>
-                        </a>
-                        <!-- <div class="collapse" id="collapseExample">
-                            <ul class="nav">
-                                <li>
-                                    <a class="profile-dropdown" href="#pablo">
-                                        <span class="sidebar-mini">MP</span>
-                                        <span class="sidebar-normal">My Profile</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a class="profile-dropdown" href="#pablo">
-                                        <span class="sidebar-mini">EP</span>
-                                        <span class="sidebar-normal">Edit Profile</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a class="profile-dropdown" href="#pablo">
-                                        <span class="sidebar-mini">S</span>
-                                        <span class="sidebar-normal">Settings</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div> -->
-                    </div>
+
                 </div>
                 <ul class="nav">
                     <li class="nav-item ">
