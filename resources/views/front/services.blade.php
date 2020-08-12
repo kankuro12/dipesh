026df3d87865ac6c65c52238a6ff16bfc1e2cd3e
krishna5816 <krishna5816@github.com>
2020-08-12 21:52:30 +0545

    changed services

--- resources/views/front/services.blade.php
+++ resources/views/front/services.blade.php
@@ -1,77 +1,28 @@
-<div id="services">
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam
-    nesciunt.<br>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat
-    facilis quas, dolorem
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam
-    nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eius quaerat dolor aut fugiat facilis quas,
-    dolorem dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam
-    nesciunt.<br>
-    dignissimos nemo illo adipisci ipsa laborum iste numquam necessitatibus dolorum rem, ipsam nesciunt.<br>
+<div id="portfolio">
+    <div style="padding-top:6rem;">
+        <div class="container">
+            <h1 class="text-center header mb-3">
+                <span>
+                    OUR PORTFOLIO
+                </span>
+            </h1>
+
+            <div class="row mt-5">
+                @foreach (\App\Service::all() as $item)
+
+                <div class="col-md-4 mb-3 ">
+                    <div class="h-100 w-100 overlay-wrapper">
+                        <img src="{{asset($item->image)}}" class="w-100 h-100">
+                        <h4 class="text-center">{{$item->title}}</h4>
+                        <div class="overlay p-3">
+                            <div class="overlay-child">
+                                {{$item->description}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 </div>
