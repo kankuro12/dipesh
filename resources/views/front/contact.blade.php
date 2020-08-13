dd9620fdff36287c79a3d6ca8fedbd7c08042a36
krishna5816 <krishna5816@github.com>
2020-08-14 05:15:01 +0545

    cta service and contact added

---
+++ resources/views/front/contact.blade.php
@@ -0,0 +1,74 @@
+<div id="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7 m-0 pr-md-0 ">
+                <div class="card h-100 w-100 p-5">
+                    <h1 class="text-center text-md-left" style="color:#22163D;font-weight:700;">CONTACT US</h1>
+                    <p class="mt-5 mb-3">
+                        <i class="fas fa-map-marker-alt fa-2x float-left"></i>
+                        Lorem ipsum dolor sit
+                        amet, consectetur
+                        adipisicing elit. </p>
+                    <p class=" mb-3"> <i class="fas fa-2x fa-phone-volume float-left"></i>9800916365 </p>
+                    <p class="mb-5"> <i class="far fa-envelope fa-2x fas float-left"></i>cms111000111@gmail.com</p>
+
+
+                    <div class="text-center">
+                        <p>
+                            <a href="http://facebook.com">
+                                <i class="fab fa-facebook-f fa-2x"></i>
+                            </a>
+                            <a href="http://twitter.com">
+                                <i class="fab fa-twitter fa-2x"></i>
+                            </a>
+                            <a href="http://instagram.com">
+                                <i class="fab fa-instagram fa-2x"></i>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col-md-5 p-md-0 ">
+                <div class="p-3 h-100" style="background:#22163D;">
+
+                    <h2 class="text-uppercase mt-3 font-weight-bold text-white">Message Us</h2>
+                    <form action="">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control mt-2" placeholder="Name" required>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control mt-2" placeholder="Address" required>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <input type="email" class="form-control mt-2" placeholder="Email" required>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <input type="number" class="form-control mt-2" placeholder="Phone" required>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <textarea class="form-control" id="exampleFormControlTextarea1"
+                                        placeholder="Message" rows="3" required></textarea>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <button class="btn btn-light" type="submit">Send Message</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/front/cta.blade.php
@@ -0,0 +1,31 @@
+<div id="cta1" class="position-relative">
+    <div class="cta-margin">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-6">
+                    <img class="cta-image w-100"
+                        src="https://www.freepnglogos.com/uploads/wedding-png/wedding-png-transparent-images-png-only-1.png"
+                        alt="" class="w-100">
+                </div>
+                <div class="col-md-6">
+                    <p class="cta-title">Urban Research
+                        Photography</p>
+                    <h1 class="cta-head">
+                        PHOTOS OF YOUR <br>LIFE
+                    </h1>
+                    <p class="cta-desc">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate consequatur blanditiis quos
+                        repudiandae error laboriosam quaerat officia nihil omnis magnam harum aspernatur necessitatibus
+                        ut natus, dolorum quod ipsa eum accusantium?
+                    </p>
+                    <div class="cta-btn-wrapper">
+                        <button class="pt-2 pb-2 pr-5 pl-5 btn bg-white cta-button">Contact Us</button>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
